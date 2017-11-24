c4c0305c30b5351a8ae2838948228856b206ddbb
pikapema <pikapema@gmail.com>
2017-11-24 18:54:12 +0100

    Init commit

---
+++ ComputerVisionAPI/ComputerVisionAPI/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ComputerVisionAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ ComputerVisionAPI/ComputerVisionAPI/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ComputerVisionAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
